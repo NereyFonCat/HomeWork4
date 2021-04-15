45932744b2b89e11748ba1056b1d9d74932a54d2
NereyFonCat <82173066+NereyFonCat@users.noreply.github.com>
2021-04-16 06:29:15 +0900

    Добавьте файлы проекта.

---
+++ HomeWork4.5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWork4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
